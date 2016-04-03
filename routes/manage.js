fa9731925433cdf1a03cb4f56517e394e11aa3d7
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-04-03 12:38:41 +0200

    added features:
    * Delete users
    * Create grops

--- routes/manage.js
+++ routes/manage.js
@@ -4,12 +4,37 @@ module.exports = function (User, Group, Permission) {
   router.get('/', function (req, res) {
     User.findAll().then(function (foundUsers) {
       Group.findAll().then(function (foundGroups) {
-        res.render('manage/index', {user: req.user, users: foundUsers, groups: foundGroups, groupMessage: req.flash('group-message')});
+        res.render('manage/index', {
+          user: req.user,
+          users: foundUsers,
+          groups: foundGroups,
+          userMessage: req.flash('user-message'),
+          userError: req.flash('user-error'),
+          groupMessage: req.flash('group-message'),
+          groupError: req.flash('group-error')
+        });
       }, function (err) {return next(err);});
     }, function (err) {return next(err);});
   });
+  router.get('/user/:username/delete', function (req, res, next) {
+    if (!req.query.redirectTo) {return res.send('Bad request');}
+    User.findOne({where: {username: req.params.username}}).then(function (foundUser) {
+      if (!foundUser) {
+        req.flash('user-error', 'User '+req.params.username+' not found');
+        return res.redirect(req.query.redirectTo);
+      }
+      foundUser.destroy().then(function () {
+        req.flash('user-message', 'User '+req.params.username+' deleted');
+        return res.redirect(req.query.redirectTo);
+      });
+    }, function (err) {return next(err);})
+  });
   router.post('/group/:name', function (req, res, next) {
     if (req.query.new) {
+      if (req.body.groupName.match(/[^\w]/g)) {
+        req.flash('group-error', 'Group Name must match \\w');
+        res.redirect('/manage#groups');
+      }
       Group.create({
         groupName: req.body.groupName,
         groupDescription: req.body.groupDescription
