84fa307a40dd9400b765f38aeaab163c19214374
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-04-03 10:23:09 +0200

    added regex to username

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,7 @@ module.exports = function (passport) {// Always send user to jade for navbar to 
     if (!req.body.username) {req.flash('signup-error', 'Sorry. Last Name can\'t be blank'); error = true;}
     if (!req.body.email) {req.flash('signup-error', 'Sorry. Email can\'t be blank'); error = true;}
     if (!req.body.password) {req.flash('signup-error', 'Sorry. Password can\'t be blank.'); error = true;}
+    if (req.body.username.match(/[^\w]/g)) {req.flash('signup-error', 'Sorry. Username must match [a-zA-Z0-9_]'); error = true;}
     if (error) {return res.redirect('/signup');}// Error empty fields
     passport.authenticate('local-signup', function(err, user) {
       if (err) {req.flash('signup-error', err); return res.redirect('/signup');}// Error inside login strategy
