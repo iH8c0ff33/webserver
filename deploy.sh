5164d3594d1ce6e3711b10e3254e774779a18482
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-06-24 17:06:24 +0200

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,19 @@ docker build -t webserver_db_image /var/webserver/db
 docker kill webserver_db
 docker rm webserver_db
 docker create \
-  -i -t \
-  --net webserver \
-  --name webserver_db \
-  webserver_db_image
+    --restart always \
+    -i -t \
+    --net webserver \
+    --name webserver_db \
+    webserver_db_image
 docker kill webserver_node
 docker rm webserver_node
 docker create \
-  -i -t \
-  --net webserver \
-  --name webserver_node \
-  --link webserver_db:database \
-  webserver_node_image
+    --restart always \
+    -i -t \
+    --net webserver \
+    --name webserver_node \
+    --link webserver_db:database \
+    webserver_node_image
 docker start webserver_db
 docker start webserver_node
