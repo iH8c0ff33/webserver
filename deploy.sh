1f06b7f34e57b39b4654b72e0586b56855e9d8c7
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-06-30 22:51:02 +0200

    should work now...

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
-docker build -t webserver_node_image /var/webserver/node
-docker build -t webserver_db_image /var/webserver/db
-docker kill webserver_db
-docker rm webserver_db
+docker build -t webserver_node_image ./node || \
+    echo " ERR: node build failed" && \
+    exit 1
+docker build -t webserver_db_image ./db || \
+    echo "ERR: db build failed" && \
+    exit 1
+docker inspect webserver_db &> /dev/null && \
+    echo "webserver_db seems to be already stopped" && \
+    docker kill webserver_db && docker rm webserver_db
 docker create \
     --restart always \
     -i -t \
     --net webserver \
     --name webserver_db \
     webserver_db_image
-docker kill webserver_node
-docker rm webserver_node
+docker inspect webserver_node &> /dev/null && \
+    echo "webserver_node seems to be already stopped" && \
+    docker kill webserver_node && docker rm webserver_node
 docker create \
     --restart always \
     -i -t \
