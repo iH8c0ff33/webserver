149e9d28c74ddaf42ca1c0cec62987e173221032
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-06-30 22:37:39 +0200

    Revert "new deploy script"
    This reverts commit 11bd50354b0b23932987213f2336ceec2ee8d62d.

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 docker build -t webserver_node_image /var/webserver/node
 docker build -t webserver_db_image /var/webserver/db
-docker inspect webserver_db && \
-    docker kill webserver_db && \
-    docker rm webserver_db
+docker kill webserver_db
+docker rm webserver_db
 docker create \
     --restart always \
     -i -t \
     --net webserver \
     --name webserver_db \
-    webserver_db_image && \
-    docker start webserver_db
-docker inspect webserver_node && \
-    docker kill webserver_node && \
-    docker rm webserver_node
+    webserver_db_image
+docker kill webserver_node
+docker rm webserver_node
 docker create \
     --restart always \
     -i -t \
     --net webserver \
     --name webserver_node \
     --link webserver_db:database \
-    webserver_node_image && \
-    docker start webserver_node
+    webserver_node_image
+docker start webserver_db
+docker start webserver_node
